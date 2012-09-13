b83d8b9b2c2b761086aca1761be88af24cbfafe8
shellman <vchakoshy@gmail.com>
2012-09-13 19:31:39 +0430

    remove title column from pin and add migration

--- pin/models.py
+++ pin/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    title = models.CharField(max_length=250, blank=True)
+    #title = models.CharField(max_length=250, blank=True)
     text = models.TextField(blank=True)
     image = models.CharField(max_length=500)
     create = models.DateField(auto_now_add=True)
