dc5cc3b3a4b1bc5a4d46c5344c7d928cde2e76ef
shellman <vchakoshy@gmail.com>
2012-09-14 18:42:53 +0430

    relative path for feed reader

--- feedreader/parse_local.py
+++ feedreader/parse_local.py
@@ -1,9 +1,13 @@
 import sys
+import os
 
-sys.path.append('/var/www/html/feedreader/')
-sys.path.append('/var/www/html/')
-sys.path.append('/home/vahid/workspace/wisgoon.com/feedreader')
-sys.path.append('/home/vahid/workspace/wisgoon.com')
+#sys.path.append('/var/www/html/feedreader/')
+#sys.path.append('/var/www/html/')
+#sys.path.append('/home/vahid/workspace/wisgoon.com/feedreader')
+#sys.path.append('/home/vahid/workspace/wisgoon.com')
+
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 
 from django.core.management import setup_environ 
 import settings_local 
