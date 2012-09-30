704df31ad780f32d3d762464d7116229fd761ad8
shellman <vchakoshy@gmail.com>
2012-09-30 21:28:12 +0330

    fix bug on parse

--- feedreader/parse.py
+++ feedreader/parse.py
@@ -4,9 +4,6 @@ import os
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 
-#sys.path.append('/var/www/html/feedreader/')
-#sys.path.append('/var/www/html/')
-
 from django.core.management import setup_environ 
 import settings
 setup_environ(settings)
@@ -14,7 +11,7 @@ setup_environ(settings)
 from rss.models import Feed
 from feedreader.parser import parse_feed
 
-feedObj = Feed.objects.filter(lock=False).all().order_by('-last_fetch')[:5]
+feedObj = Feed.objects.filter(lock=False).all().order_by('last_fetch')[:5]
 
 for i in feedObj:
     parse_feed(i)
