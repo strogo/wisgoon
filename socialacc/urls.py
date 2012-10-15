2baa4091e1e508f051de8d53563a84d3d78f2309
shellman <vchakoshy@gmail.com>
2012-10-16 01:51:03 +0330

    change on file

--- socialacc/urls.py
+++ socialacc/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('socialacc.views',
-    url(r'^$', 'home', name='social-home'),
+    #url(r'^$', 'home', name='social-home'),
     url(r'^import_contacts/$', 'import_contacts', name="social-import"),
 
 )
