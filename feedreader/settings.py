b39270c79a07fa1d5ee6911d03f04d4302db76f2
root <root@doit1.rackset.com>
2012-12-07 08:48:40 -0500

    change on server

--- feedreader/settings.py
+++ feedreader/settings.py
@@ -17,7 +17,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'feedreader',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'kY6jSW6cmZIDT',                  # Not used with sqlite3.
+        'PASSWORD': '-)**Z{QT',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
