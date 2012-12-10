a0281e97a8aefddeda78325b3500e82c95afcd80
shellman <vchakoshy@gmail.com>
2012-12-11 00:29:40 +0330

    limit 20 for notify

--- pin/views.py
+++ pin/views.py
@@ -393,7 +393,7 @@ def like(request, item_id):
 
 def show_notify(request):
     Notify.objects.filter(user_id=request.user.id, seen=False).update(seen=True)
-    notif = Notify.objects.all().filter(user_id=request.user.id).order_by('-id')
+    notif = Notify.objects.all().filter(user_id=request.user.id).order_by('-id')[:20]
     return render_to_response('pin/notify.html',{'notif':notif})
 
 def tag_complete(request):
