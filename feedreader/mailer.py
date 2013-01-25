8fd59b4d2ee42b247806f2b28dd7ba690458ca53
shellman <vchakoshy@gmail.com>
2013-01-25 19:22:28 +0330

    add users for mailer

--- feedreader/mailer.py
+++ feedreader/mailer.py
@@ -3,6 +3,7 @@
 import smtplib
 import sys
 import os
+from django.contrib.auth.models import User
 
 sys.path.append(os.path.dirname(__file__))
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
@@ -38,31 +39,32 @@ msg['To'] = you
 
 # Create the body of the message (a plain-text and an HTML version).
 #email_content = get_template('mailer/mail.html')
+users = User.objects.all()
+for user in users:
+    notify = Notify.objects.all().filter(user_id=user.id, seen=0)
 
-notify = Notify.objects.all().filter(seen=0)
-
-email_content = render_to_string('mailer/notify.html',{'notify':notify})
-#email_content = open('templates/mailer/mail.html', 'r').read()
-#f = open('out.html','w+')
-#f.write(email_content.encode('utf-8'))
-#f.close()
-
-text = stripIt(email_content)
-html = email_content
-
-# Record the MIME types of both parts - text/plain and text/html.
-part1 = MIMEText(text, 'plain', _charset='utf-8')
-part2 = MIMEText(html, 'html', _charset='utf-8')
-
-# Attach parts into message container.
-# According to RFC 2046, the last part of a multipart message, in this case
-# the HTML message, is best and preferred.
-msg.attach(part1)
-msg.attach(part2)
-
-# Send the message via local SMTP server.
-s = smtplib.SMTP('localhost')
-# sendmail function takes 3 arguments: sender's address, recipient's address
-# and message to send - here it is sent as one string.
-s.sendmail(me, you, msg.as_string())
-s.quit()
+    email_content = render_to_string('mailer/notify.html',{'notify':notify})
+    #email_content = open('templates/mailer/mail.html', 'r').read()
+    #f = open('out.html','w+')
+    #f.write(email_content.encode('utf-8'))
+    #f.close()
+    
+    text = stripIt(email_content)
+    html = email_content
+    
+    # Record the MIME types of both parts - text/plain and text/html.
+    part1 = MIMEText(text, 'plain', _charset='utf-8')
+    part2 = MIMEText(html, 'html', _charset='utf-8')
+    
+    # Attach parts into message container.
+    # According to RFC 2046, the last part of a multipart message, in this case
+    # the HTML message, is best and preferred.
+    msg.attach(part1)
+    msg.attach(part2)
+    
+    # Send the message via local SMTP server.
+    s = smtplib.SMTP('localhost')
+    # sendmail function takes 3 arguments: sender's address, recipient's address
+    # and message to send - here it is sent as one string.
+    s.sendmail(me, you, msg.as_string())
+    s.quit()
