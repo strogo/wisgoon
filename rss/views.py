245d8fc6320cbe6864d7eed48d13e0c5120061ae
shellman <vchakoshy@gmail.com>
2013-02-21 01:19:45 +0330

    fix bug

--- rss/views.py
+++ rss/views.py
@@ -323,7 +323,7 @@ def search_query(query, offset=0, sort=1, has_image=-1):
         """ order to date  """
         cl.SetSortMode(SPH_SORT_ATTR_DESC, 'date_added')
     
-    if has_image in (-1,0,1):
+    if has_image != -1 and  has_image in (0,1):
         cl.SetFilter('has_image', [has_image])
 
     
