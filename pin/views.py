13803b7d77eab34036d0f8a4d965da803c2ccbb0
shellman <vchakoshy@gmail.com>
2013-06-18 00:40:49 +0430

    change top user

--- pin/views.py
+++ pin/views.py
@@ -101,7 +101,7 @@ def popular(request, interval = ""):
     
 
 def topuser(request):
-    top_user=Profile.objects.all().order_by('-score')[:50]
+    top_user=Profile.objects.all().order_by('-score')[:150]
 
     return render(request, 'pin/topuser.html', {'top_user': top_user})
 
