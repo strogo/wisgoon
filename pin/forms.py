1b5ae549f233aeed010045375e71d849411aa7a9
shellman <vchakoshy@gmail.com>
2013-07-14 01:07:41 +0430

    exclude show in default from form

--- pin/forms.py
+++ pin/forms.py
@@ -7,7 +7,7 @@ class PinForm(ModelForm):
     class Meta:
         model=Post
         exclude = ('user', 'like', 'timestamp', 'status','device','hash',
-        'actions','is_ads','view')
+        'actions','is_ads','view', 'show_in_default')
         
 class PinDirectForm(forms.Form):
     image = forms.ImageField()
@@ -18,5 +18,5 @@ class PinUpdateForm(ModelForm):
     class Meta:
         model=Post
         exclude = ('user', 'like', 'timestamp',
-        'image','status','device','hash', 'actions','view' )
+        'image','status','device','hash', 'actions','view','show_in_default' )
         
