72b90c30e1616e003a7d6643dbb5aaf84d82694a
shellman <vchakoshy@gmail.com>
2013-07-27 05:10:03 +0430

    add notif date for comments

--- pin/models.py
+++ pin/models.py
@@ -240,6 +240,7 @@ def send_notif(user, type, post, actor, seen=False):
     notif, created = Notif.objects.get_or_create(user=user, type=type, post=post)
 
     notif.seen = seen
+    notif.date = datetime.datetime.now()
     notif.save()
 
     Notif_actors.objects.get_or_create(notif=notif, actor=actor)
