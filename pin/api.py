c3286b76107e39cf0c8e4d3767dfc2146cc28e91
shellman <vchakoshy@gmail.com>
2013-08-11 08:47:55 +0430

    change

--- pin/api.py
+++ pin/api.py
@@ -263,7 +263,7 @@ class PostResource(ModelResource):
 
         if int(self.just_image) == 1:
             for key in ['user', 'user_name', 'user_avatar', 'url', 'like', 'like_with_user', 
-                        'cnt_comment', 'category', 'text', 'image', 'likers']:
+                        'cnt_comment', 'category', 'text', 'image', 'likers', 'resource_uri']:
                 del(bundle.data[key])
 
             return bundle        
