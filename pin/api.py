2fedbbd97b7ecbf534414681002c6f651e8b248f
vahid <vchakoshy@gmail.com>
2014-01-10 21:45:31 +0330

    remove relateed

--- pin/api.py
+++ pin/api.py
@@ -193,7 +193,7 @@ class PostResource(ModelResource):
     dispatch_exec = False
 
     class Meta:
-        queryset = Post.objects.select_related().filter(status=1).order_by('-id')
+        queryset = Post.objects.filter(status=1).order_by('-id')
         resource_name = 'post'
         allowed_methods = ['get']
         paginator_class = Paginator
