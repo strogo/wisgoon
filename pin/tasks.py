e16c3d3e454b449e5ee25737df4c78bb76c79ed8
vahid <vchakoshy@gmail.com>
2014-02-16 00:46:00 +0330

    change on file

--- pin/tasks.py
+++ pin/tasks.py
@@ -3,26 +3,11 @@ from __future__ import absolute_import
 
 from datetime import datetime
 
-from pin.celery import app
+#from pin.celery import app
 from pin.models import Notif, Notif_actors
 
 
-@app.task
-def add(x, y):
-    return x + y
-
-
-@app.task
-def mul(x, y):
-    return x * y
-
-
-@app.task
-def xsum(numbers):
-    return sum(numbers)
-
-
-@app.task
+#@app.task
 def send_notif(user, type, post, actor, seen=False):
     notif, created = Notif.objects.get_or_create(user=user, type=type, post_id=post)
 
