c3f3c7e2f96ee17afcda053e0f303860af255b40
karoon <vchakoshy@gmail.com>
2014-05-03 00:09:49 +0430

    remove send _notif

--- pin/tasks.py
+++ pin/tasks.py
@@ -9,14 +9,15 @@ from pin.models import Notif, Notif_actors, Notifbar
 
 #@app.task
 def send_notif(user, type, post, actor, seen=False):
+    """
     notif, created = Notif.objects.get_or_create(user=user, type=type, post_id=post)
 
     notif.seen = seen
     notif.date = datetime.now()
     notif.save()
 
-    Notif_actors.objects.get_or_create(notif=notif, actor=actor)
-    return notif
+    Notif_actors.objects.get_or_create(notif=notif, actor=actor)"""
+    return True
 
 
 def send_notif_bar(user, type, post, actor, seen=False):
