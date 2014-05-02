5b5020c2176536707a33f447b03b83c4f229a844
karoon <vchakoshy@gmail.com>
2014-05-03 00:09:12 +0430

    disable notif

--- pin/models.py
+++ pin/models.py
@@ -371,13 +371,13 @@ class Notif(models.Model):
         post = comment.object_pk
 
         if comment.user != post.user:
-            notif = send_notif(user=post.user, type=2, post=post.id, actor=comment.user)
+            #notif = send_notif(user=post.user, type=2, post=post.id, actor=comment.user)
             notif = send_notif_bar(user=post.user, type=2, post=post.id, actor=comment.user)
 
-        for notif in Notif.objects.filter(type=2, post=post):
-            for act in Notif_actors.objects.filter(notif=notif):
-                if act.actor != comment.user:
-                    send_notif(user=act.actor, type=2, post=post.id, actor=comment.user)
+        #for notif in Notif.objects.filter(type=2, post=post):
+        #    for act in Notif_actors.objects.filter(notif=notif):
+        #        if act.actor != comment.user:
+        #            send_notif(user=act.actor, type=2, post=post.id, actor=comment.user)
 
                 #print act.actor_id
 
