8c679b230f17a3bb968052f04ed14a78bd862560
karoon <vchakoshy@gmail.com>
2014-06-02 21:20:20 +0430

    fix bug

--- pin/views2_api.py
+++ pin/views2_api.py
@@ -387,7 +387,7 @@ def notif(request):
         }
 
         ar = []
-        for ac in p.last_actors:
+        for ac in p.last_actors():
             ar.append([
                 ac,
                 AuthCache.get_username(ac)[1:],
