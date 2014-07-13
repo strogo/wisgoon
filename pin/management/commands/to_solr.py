68e811581ea3f11d5e30fcbaec15ffcc0026f3dd
karoon <vchakoshy@gmail.com>
2014-07-13 19:15:53 +0430

    fix xbug

--- pin/management/commands/to_solr.py
+++ pin/management/commands/to_solr.py
@@ -11,35 +11,44 @@ from user_profile.models import Profile
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-    	solr = pysolr.Solr('http://localhost:8983/solr/wisgoon_user', timeout=10)
+        solr = pysolr.Solr('http://localhost:8983/solr/wisgoon_user', timeout=10)
         print "tuning execute"
-        
+
         cur_id = cache.get('cur_solr_user_id', 0)
         print "cur id is", cur_id
 
+        u = None
         all_rows = []
 
         for u in User.objects.values('id', 'username').filter(id__gt=cur_id)[:1000]:
-        	try:
-	        	p = Profile.objects.values('name').get(user_id=u['id'])
-	        	row = {
-	        		"id": u['id'],
-	        		"name_s": p['name'],
-	        		"username_s": u['username'],
-	        		"name_t": p['name'],
-	        		"username_t": u['username'],
-	        	}
-	        	
-	        except Profile.DoesNotExist:
-	        	row = {
-	        		"id": u['id'],
-	        		"username_s": u['username'],
-	        		"username_t": u['username'],
-	        	}
+            try:
+                p = Profile.objects.values('name', 'score').get(user_id=u['id'])
+                row = {
+                    "id": u['id'],
+                    "name_s": p['name'],
+                    "username_s": u['username'],
+                    "name_t": p['name'],
+                    "username_t": u['username'],
+                    "score_i": p['score'],
+                }
+                
+            except Profile.DoesNotExist:
+                row = {
+                    "id": u['id'],
+                    "username_s": u['username'],
+                    "username_t": u['username'],
+                    "score_i": 0,
+                }
 
-	        all_rows.append(row)
-	        cache.set('cur_solr_user_id', u['id'], 86400)
-        	
+            all_rows.append(row)
+            cache.set('cur_solr_user_id', u['id'], 86400)
+
+        if u:
+            print "hey u"
+        else:
+            cache.delete('cur_solr_user_id')
+            print "none"
+            
         solr.add(all_rows)
 
         
