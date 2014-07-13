5a0a47b063e01288613b6a00ecf93a780030ad07
karoon <vchakoshy@gmail.com>
2014-07-13 20:16:05 +0430

    check for following bug

--- pin/views_user.py
+++ pin/views_user.py
@@ -84,16 +84,16 @@ def follow(request, following, action):
                 .all().delete()
         elif created:
             posts = Post.objects.filter(user=following, status=1)[:100]
-            with transaction.commit_on_success():
-                for post in posts:
-                    stream = Stream(post=post,
-                                    user=request.user,
-                                    date=post.timestamp,
-                                    following=following)
-                    try:
-                        stream.save()
-                    except IntegrityError:
-                        print "duplicate in stream"
+            #with transaction.commit_on_success():
+            for post in posts:
+                stream = Stream(post=post,
+                                user=request.user,
+                                date=post.timestamp,
+                                following=following)
+                try:
+                    stream.save()
+                except IntegrityError:
+                    print "duplicate in stream"
         return HttpResponseRedirect(reverse('pin-user', args=[following.id]))
     except User.DoesNotExist:
         return HttpResponseRedirect(reverse('pin-user', args=[following.id]))
