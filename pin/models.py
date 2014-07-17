9792c04bdd8776336a00275d60f98ea681cd1c73
karoon <vchakoshy@gmail.com>
2014-07-18 04:01:04 +0430

    improve count of hot posts

--- pin/models.py
+++ pin/models.py
@@ -136,7 +136,7 @@ class Post(models.Model):
     @classmethod
     def hot(self, post_id, amount=1):
         r_server.zincrby('hot', int(post_id), amount=amount)
-        r_server.zremrangebyrank('hot', 0, -101)
+        r_server.zremrangebyrank('hot', 0, -1001)
 
     @classmethod
     def add_to_set(self, set_name, post, set_cat=True):
