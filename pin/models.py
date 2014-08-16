276934208fc6dcffb10976fbf0f8666b4dbb6312
shellman <vchakoshy@gmail.com>
2014-08-16 18:43:19 +0430

    add hot default post

--- pin/models.py
+++ pin/models.py
@@ -156,6 +156,11 @@ class Post(models.Model):
 
     @classmethod
     def get_hot(self, values=False):
+        if values:
+            post = Post.objects\
+                .values(*self.NEED_KEYS)\
+                .filter(id=2416517)
+
         h = r_server.zrange('hot', 0, 0, withscores=True, desc=True)
         if h[0][1] > 110:
             r_server.sadd('hottest', h[0][0])
