b0ecb1f18de9820b296ac704397f3443db672cff
shellman <vchakoshy@gmail.com>
2014-08-22 21:04:17 +0430

    add resource uri

--- pin/views2_api.py
+++ pin/views2_api.py
@@ -539,7 +539,8 @@ def notif(request):
         o['likers'] = None
         o['like_with_user'] = False
 
-        o['resource_uri'] = "/pin/api/notif/notify/%d/" % cur_p['id']
+        #o['resource_uri'] = "/pin/api/notif/notify/%d/" % cur_p['id']
+        o['resource_uri'] = "/pin/api/post/%d/" % p['id']
 
         if cur_user:
             o['like_with_user'] = Likes.user_in_likers(post_id=cur_p['id'], user_id=cur_user)
