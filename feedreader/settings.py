f7f94b585e686198bb3d80467822819df929a80a
shellman <vchakoshy@gmail.com>
2014-11-08 15:31:39 +0330

    add mailserver

--- feedreader/settings.py
+++ feedreader/settings.py
@@ -10,7 +10,7 @@ DATABASES = {
         'NAME': 'feedreader',
         'USER': 'root',
         'PASSWORD': '-)**Z{QT',
-        'HOST': '79.127.125.149',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
@@ -18,6 +18,11 @@ DATABASES = {
 ALLOWED_HOSTS = ['www.wisgoon.com', '*.wisgoon.com', 'wisgoon.com']
 EMAIL_HOST = "wisgoon.com"
 DEFAULT_FROM_EMAIL = "info@wisgoon.com"
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'mail.wisgoon.com'
+EMAIL_HOST_USER = 'info@wisgoon.com'
+EMAIL_HOST_PASSWORD = '-)**Z{QT'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
 JOHNNY_MIDDLEWARE_KEY_PREFIX = 'wis_cac2'
