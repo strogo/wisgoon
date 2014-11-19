dea2027156c957abd52ce01576b6fe5c9030c39a
shellman <vchakoshy@gmail.com>
2014-11-19 12:17:48 +0330

    add approve

---
+++ pin/management/commands/approve.py
@@ -0,0 +1,13 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from pin.models import Post
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print "approve posts"
+        
+        for p in Post.objects.filter(status=Post.PENDING)[:3]:
+            p.approve()
+
+        
