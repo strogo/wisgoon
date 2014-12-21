ef8e4afe009e3380197199d1d56ca42d1bcaaa38
shellman <vchakoshy@gmail.com>
2014-12-21 17:27:49 +0330

    filter by admin

--- pin/views.py
+++ pin/views.py
@@ -442,7 +442,10 @@ def item(request, item_id):
     #     post.comments = Comments.objects.filter(object_pk=post)
     #     post.likes = Likes.objects.filter(post=post).order_by('ip')[:10]
     # else:
-    post.comments = Comments.objects.filter(object_pk=post, is_public=True)
+    if request.user.is_superuser:
+        post.comments = Comments.objects.filter(object_pk=post, is_public=True)
+    else:
+        post.comments = Comments.objects.filter(object_pk=post)
     str_likers = "web_likes_%s" % post.id
     csl = cache.get(str_likers)
     if csl:
