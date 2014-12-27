3870f369f1f70f7b4e03dc508b1e67c9a8682c0d
shellman <vchakoshy@gmail.com>
2014-12-27 19:07:22 +0330

    add pin like

--- feedreader/media/js2/pin-user.js
+++ feedreader/media/js2/pin-user.js
@@ -138,7 +138,7 @@ $('body').on('click', '.btn_like',function(){
             
             var o = jQuery.parseJSON(ret);
             
-            obj.html('<i class="icon-heart"></i>'+o[0].likes);
+            obj.html('<i class="glyphicon glyphicon-heart"></i>'+o[0].likes);
             obj.removeClass('disabled');
             
             if (o[0].user_act == 1){
