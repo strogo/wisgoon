df86e2d306d4290af63083b1efb75e2dea82e02f
shellman <vchakoshy@gmail.com>
2014-12-29 17:52:28 +0330

    fix bug

--- pin/views.py
+++ pin/views.py
@@ -37,7 +37,7 @@ def home(request):
     if request.is_ajax():
         if latest_items.exists():
             return render(request,
-                          'pin2/_items_2.html',
+                          'pin2/_items_2_1.html',
                           {'latest_items': latest_items})
         else:
             return HttpResponse(0)
