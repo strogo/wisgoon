4d96ae54bf8c54d48a8eab5e2f5854acad75245d
shellman <vchakoshy@gmail.com>
2015-01-03 12:57:36 +0330

    change on feedback

--- contactus/forms.py
+++ contactus/forms.py
@@ -1,10 +1,12 @@
+#-*- coding: utf-8 -*-
 from django.forms.models import ModelForm
 from contactus.models import Feedback
 
 from captcha.fields import CaptchaField
 
+
 class FeedbackFrom(ModelForm):
-    captcha = CaptchaField()
+    captcha = CaptchaField('کد امنیتی')
+
     class Meta:
-        model=Feedback
-        
+        model = Feedback
