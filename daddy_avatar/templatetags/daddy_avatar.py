834c812bdb1e1740754a8202a8ad895af3b24cca
shellman <vchakoshy@gmail.com>
2015-01-05 13:09:31 +0330

    change on file

--- daddy_avatar/templatetags/daddy_avatar.py
+++ daddy_avatar/templatetags/daddy_avatar.py
@@ -67,10 +67,13 @@ def get_avatar(user, size=200):
             if profile.avatar:
                 # print "step 1.2.1"
                 t_size = '%sx%s' % (size, size)
-                im = get_thumbnail(profile.avatar, t_size, crop='center', quality=99)
-                ava_dict[size] = im.url
-                cache.set(ava_str, ava_dict, 60 * 60 * 1)
-                return im.url
+                try:
+                    im = get_thumbnail(profile.avatar, t_size, crop='center', quality=99)
+                    ava_dict[size] = im.url
+                    cache.set(ava_str, ava_dict, 60 * 60 * 1)
+                    return im.url
+                except Exception, e:
+                    print "daddy_avatar line 96: ", str(e)
     # except Exception, e:
     #     print str(e)
     else:
@@ -88,7 +91,7 @@ def get_avatar(user, size=200):
                 cache.set(ava_str, ava_dict, 60 * 60 * 1)
                 return im.url
             except Exception, e:
-                print "daddy_avatar line 91: ", str(e)
+                print "daddy_avatar line 94: ", str(e)
 
     glob_avatar = daddy_avatar(user.email, size)
     ava_dict[size] = glob_avatar
