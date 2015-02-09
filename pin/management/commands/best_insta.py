aa56c1b80681234c523a18b0e09235e945e1585b
shellman <vchakoshy@gmail.com>
2015-02-09 20:38:35 +0330

    change on file

--- pin/management/commands/best_insta.py
+++ pin/management/commands/best_insta.py
@@ -11,12 +11,12 @@ from pin.tools import create_filename
 MEDIA_ROOT = settings.MEDIA_ROOT
 
 
-def get_from_insta():
+def get_from_insta(insta_user_id, cat):
     client_id = "ecb7cbd35a11467fb2cf558583a44047"
     client_secret = "74e85dfb6b904ac68139a93a9b047247"
     api = client.InstagramAPI(client_id=client_id, client_secret=client_secret)
 
-    recent_media, next_ = api.user_recent_media(user_id="1462129775", count=5)
+    recent_media, next_ = api.user_recent_media(user_id=insta_user_id, count=5)
 
     for media in recent_media:
         if media.type == "image":
@@ -41,7 +41,7 @@ def get_from_insta():
             model.timestamp = time.time()
             model.user_id = 636690
             model.status = Post.APPROVED
-            model.category_id = 7
+            model.category_id = cat
             model.save()
 
             InstaMeta.objects.create(post=model.id, insta_id=id)
@@ -49,4 +49,4 @@ def get_from_insta():
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        get_from_insta()
+        get_from_insta(insta_user_id="1462129775", cat=7)
