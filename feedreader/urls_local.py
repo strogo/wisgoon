e88eaa6c44f6f096849941c57914552adc9bb308
shellman <vchakoshy@gmail.com>
2015-02-10 15:42:33 +0330

    change on file

--- feedreader/urls_local.py
+++ feedreader/urls_local.py
@@ -10,9 +10,9 @@ urlpatterns += patterns('',
 )
 
 urlpatterns += patterns('pin.views',
-    url(r'^(?P<user_namefl>.*)/followers/$', 'absuser_followers', name='pin-user-followers'),
-    url(r'^(?P<user_namefg>.*)/following/$', 'absuser_friends', name='pin-user-following'),
-    url(r'^(?P<user_namel>.*)/likes/$', 'absuser_like', name='pin-user-like'),
-    url(r'^(?P<user_name>.*)/$', 'absuser', name='pin-user'),
+    url(r'^(?P<user_namefl>.*)/followers/$', 'absuser_followers', name='pin-absuser-followers'),
+    url(r'^(?P<user_namefg>.*)/following/$', 'absuser_friends', name='pin-absuser-following'),
+    url(r'^(?P<user_namel>.*)/likes/$', 'absuser_like', name='pin-absuser-like'),
+    url(r'^(?P<user_name>.*)/$', 'absuser', name='abspin-user'),
     # url(r'^(?P<user_namef>.*)/friends/$', 'user_friends', name='pin-user-friends'),
 )
