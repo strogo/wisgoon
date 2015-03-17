92ba979ad9b9d24ad4a8903354c3fc632e536e50
Your Name <vchakoshy@gmail.com>
2015-03-17 17:22:19 +0330

    change on file

--- push.sh
+++ push.sh
@@ -3,11 +3,17 @@
 send(){
 	git push origin devel
 	ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel && touch reload"
+
+	git push origin2 devel
+	ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel && touch reload"
 }
 
 jsend(){
 	git push origin devel
 	ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel"
+
+	git push origin2 devel
+	ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel"
 }
 
 case $1 in
