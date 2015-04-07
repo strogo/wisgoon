243ad6b7b6879a4e0c0177c297b5a274c8318fd5
Your Name <vchakoshy@gmail.com>
2015-04-07 14:22:47 +0430

    fix signal

--- pin/signals.py
+++ pin/signals.py
@@ -3,7 +3,7 @@ from django.db.models.loading import get_model
 from haystack import signals
 
 # from models import Channel, Video
-from pin.models import Post
+from pin.models import Post, Comments
 from user_profile.models import Profile
 
 
@@ -11,6 +11,8 @@ class MySignalProcessor(signals.BaseSignalProcessor):
     def setup(self):
         models.signals.post_save.connect(self.handle_save, sender=Post)
         models.signals.post_save.connect(self.handle_save, sender=Profile)
+        models.signals.post_save.connect(self.handle_save, sender=Comments)
         
         models.signals.post_delete.connect(self.handle_delete, sender=Post)
         models.signals.post_delete.connect(self.handle_delete, sender=Profile)
+        models.signals.post_delete.connect(self.handle_delete, sender=Comments)
