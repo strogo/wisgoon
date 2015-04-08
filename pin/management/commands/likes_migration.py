9e46b2fed1689b310b6721a85e1baab8800289cb
Your Name <vchakoshy@gmail.com>
2015-04-08 14:29:10 +0430

    change on file

--- pin/management/commands/likes_migration.py
+++ pin/management/commands/likes_migration.py
@@ -6,6 +6,6 @@ from pin.models_redis import LikesRedis
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for l in Likes.objects.values_list('post_id', flat=True).distinct()[:5000]:
+        for l in Likes.objects.values_list('post_id', flat=True).distinct()[:2000]:
             print l
             LikesRedis(post_id=l)
