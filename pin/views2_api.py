808d972f3e5bbe79d6a48cbcac8efda256ed47f6
Your Name <vchakoshy@gmail.com>
2015-07-21 08:28:22 +0430

    fix bug

--- pin/views2_api.py
+++ pin/views2_api.py
@@ -1528,7 +1528,7 @@ def get_phone_data(request):
     if imei:
         for pdq in PhoneData.objects.filter(imei=imei):
             if not pdq.user.is_active:
-                Log.ban_by_imei(actor=user)
+                Log.ban_by_imei(actor=user, text=pdq.user.username)
                 User.objects.filter(pk=user.id).update(is_active=False)
 
     upd, created = PhoneData.objects.get_or_create(user=user)
