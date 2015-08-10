c4ec5b522d7290f2c88387c09eedb6138636d9af
Your Name <vchakoshy@gmail.com>
2015-08-10 10:06:41 +0430

    remove cache from notification

--- pin/tasks.py
+++ pin/tasks.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from django.core.cache import cache
 
 from django.conf import settings
 
@@ -17,8 +16,6 @@ def send_notif(user, type, post, actor, seen=False):
 
 
 def send_notif_bar(user, type, post, actor, seen=False, post_image=None):
-    notif_cache_key = "notif_v112_%d" % (int(user))
-    cache.delete(notif_cache_key)
     try:
         if settings.USE_CELERY:
             notif_send.delay(user, type, post, actor, seen=False,
--- pin/views2_api.py
+++ pin/views2_api.py
@@ -619,10 +619,10 @@ def notif(request):
     if not cur_user:
         return HttpResponse("problem", cur_user)
 
-    notif_cache_key = "notif_v112_%d" % (int(cur_user))
-    c_data = cache.get(notif_cache_key)
-    if c_data:
-        pass
+    # notif_cache_key = "notif_v112_%d" % (int(cur_user))
+    # c_data = cache.get(notif_cache_key)
+    # if c_data:
+    #     pass
         # print "get from cache", notif_cache_key
         # return HttpResponse(c_data, content_type="application/json")
 
