020ace47c5db811b1aee5630e17257c97367ae7a
Your Name <vchakoshy@gmail.com>
2015-08-16 19:12:28 +0430

    add category cache layer

--- pin/cacheLayer.py
+++ pin/cacheLayer.py
@@ -2,7 +2,7 @@ from django.core.cache import cache
 
 
 class UserDataCache(object):
-    key_name = "C:L:U:%(user_id)s"
+    key_name = "C:L:U:%(user_id)s"  # cache layer user 1
     ttl = 86400
 
     cache_in_class = {}
@@ -24,3 +24,32 @@ class UserDataCache(object):
         cache.set(keyname, username, cls.ttl)
         cls.cache_in_class[keyname] = username
         return username
+
+
+class CategoryDataCache(object):
+    key_name = "C:L:C:%(category_id)s"  # cache layer category 1
+    ttl = 86400
+
+    cache_in_class = {}
+
+    @classmethod
+    def get_cat_json(cls, category_id):
+        keyname = cls.key_name % {'category_id': category_id}
+        if keyname in cls.cache_in_class:
+            return cls.cache_in_class[keyname]
+        jcc = cache.get(keyname)
+        if jcc:
+            return jcc
+
+        from pin.models import Category
+
+        cat = Category.objects.get(id=category_id)
+        cat_json = {
+            'id': cat.id,
+            'image': cat.image.url,
+            'resource_uri': "/pin/apic/category/" + str(cat.id) + "/",
+            'title': cat.title,
+        }
+        cache.set(keyname, cat_json, cls.ttl)
+        cls.cache_in_class[keyname] = cat_json
+        return cat_json
