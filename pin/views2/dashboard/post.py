42c9f87743478110863d0cde96110179de5c57b0
shohreh <shohre.shadvalian.1@gmail.com>
2015-11-10 03:24:56 +0330

    new theme completed

--- pin/views2/dashboard/post.py
+++ pin/views2/dashboard/post.py
@@ -7,3 +7,15 @@ def home(request):
 
 def reported(request):
     return render(request, 'dashboard/post/reported.html')
+
+def user_activity(request):
+    return render(request, 'dashboard/post/user_activity.html')
+    
+def logs(request):
+    return render(request, 'dashboard/post/logs.html')
+
+def ads(request):
+    return render(request, 'dashboard/post/ads.html')
+
+def catregory(request):
+    return render(request, 'dashboard/post/category.html')
