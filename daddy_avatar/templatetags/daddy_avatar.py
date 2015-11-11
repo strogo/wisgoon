a916e530d522cc6c4b37f97c96d21b3c2257128c
vchakoshy <vchakoshy@gmail.com>
2015-11-11 16:35:11 +0330

    fix bug

--- daddy_avatar/templatetags/daddy_avatar.py
+++ daddy_avatar/templatetags/daddy_avatar.py
@@ -31,21 +31,36 @@ def get_avatar(user, size=165):
     if isinstance(user, (int, long)):
         u = UserGeneric()
         u.id = int(user)
-
+        # # user = User.objects.only('email').get(pk=user)
+        # user_str = "user_%d" % (user)
+        # user_cache = cache.get(user_str)
+        # if user_cache:
+        #     user = user_cache
+        # else:
+        #     try:
+        #         user = User.objects.only('email').get(pk=user)
+        #         cache.set(user_str, user, 60 * 60 * 24)
+        #     except:
+        #         return 'None'
     if isinstance(user, User):
         u = user
 
     user_id = u.id
 
     ava_str = "avatar3210u_%d" % (user_id)
-
+    # ava_dict = {}
+    # try:
     ava_dict = cache.get(ava_str)
-
+    # print "ava cache is:", ava_dict, ava_str, size
     if ava_dict:
+        # print "step 1"
 
         if size in ava_dict:
+            # print "step 1.1"
+            # print "get avatar from cache", ava_dict
             return ava_dict[size]
         else:
+            # print "step 1.2"
             try:
                 profile = Profile.objects.only('avatar').get(user_id=user_id)
             except Profile.DoesNotExist:
@@ -53,6 +68,7 @@ def get_avatar(user, size=165):
 
             if profile:
                 if profile.avatar:
+                    # print "step 1.2.1"
                     t_size = '%sx%s' % (size, size)
                     try:
                         im = get_thumbnail(profile.avatar, t_size, crop='center', quality=99)
@@ -61,15 +77,19 @@ def get_avatar(user, size=165):
                         return im.url
                     except Exception, e:
                         print "daddy_avatar line 96: ", str(e)
-
+    # except Exception, e:
+    #     print str(e)
     else:
+        # print "step 2"
         ava_dict = {}
+        # print "step 2.1"
         try:
             profile = Profile.objects.only('avatar').get(user_id=user_id)
         except Profile.DoesNotExist:
             profile = None
 
         if profile:
+            # print "step 2.2.1"
             t_size = '%sx%s' % (size, size)
             try:
                 im = get_thumbnail(profile.avatar, t_size, crop='center', quality=99)
@@ -78,6 +98,7 @@ def get_avatar(user, size=165):
                 return im.url
             except Exception, e:
                 pass
+                # print "daddy_avatar line 94: ", str(e)
 
     glob_avatar = daddy_avatar("", size)
     ava_dict[size] = glob_avatar
