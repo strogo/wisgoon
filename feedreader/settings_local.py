f4e9d449619f96d7f3fa152b1d4d6bb55700ea0e
chalist <mohammad.molana@gmail.com>
2015-11-14 16:42:56 +0330

    change static version

--- feedreader/settings_local.py
+++ feedreader/settings_local.py
@@ -6,7 +6,7 @@ DEBUG = True
 THUMBNAIL_DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 REPORT_TYPE = {'PIN': 1, 'COMMENT': 2, 'RSS': 3}
-STATIC_VERSION = '0.3.0'
+STATIC_VERSION = '0.3.1'
 SITE_ROOT = os.path.dirname(__file__)
 ADMINS = (
     ('bugs', 'bugs@wisgoon.com'),
