fe8869a3db2ac047d925897eab540c24ceeea5e3
vchakoshy <vchakoshy@gmail.com>
2015-11-16 20:15:45 +0330

    change on file

--- pin/management/commands/avatar_migration.py
+++ pin/management/commands/avatar_migration.py
@@ -2,11 +2,13 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 
 from user_profile.models import Profile
+from daddy_avatar.templatetags.daddy_avatar import get_avatar
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for p in Profile.objects.filter(avatar__contains="avatars/2"):
+        for p in Profile.objects.only('user').filter(avatar__contains="avatars/2"):
+            get_avatar(p.user_id, size=64)
             p.store_avatars(update_model=True)
             from pin.tasks import migrate_avatar_storage
             migrate_avatar_storage.delay(profile_id=p.id)
