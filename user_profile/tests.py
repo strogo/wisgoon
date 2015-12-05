e27dbcd48c246d7f8939fb3c3cbea710379071f3
amir abbas ghorbani <a.abbasghorbani@gmail.com>
2015-12-05 18:02:53 +0330

    create unit test for profie

--- user_profile/tests.py
+++ user_profile/tests.py
@@ -1,11 +1,6 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+# coding: utf-8
 import unittest
-from models import Profile, CreditLog
+from models import Profile
 from django.contrib.auth.models import User
 
 
@@ -14,10 +9,39 @@ class ProfileTest(unittest.TestCase):
     def setUp(self):
         self.amir, created = User.objects\
             .get_or_create(username='amir', email='a.ab@yahoo.com', password='1')
+        self.profile = Profile.objects.get(user=self.amir)
 
     def tearDown(self):
         super(ProfileTest, self).tearDown()
 
     def test_create_profile(self):
-        self.profile = Profile.objects.get(user=self.amir)
+        self.profile.credit = 500
+        self.profile.level = 1
+        self.profile.save()
+
         self.assertIsInstance(self.profile, Profile)
+        self.assertEqual(self.profile.credit, 500)
+        self.assertEqual(self.profile.level, 1)
+        self.assertFalse(self.profile.is_police())
+
+    def test_inc_credit(self):
+        self.profile.inc_credit(1000)
+        self.profile = Profile.objects.get(user=self.amir)
+        self.assertEqual(self.profile.credit, 1500)
+
+    def dec_credit(self):
+        self.profile.inc_credit(1000)
+        self.profile = Profile.objects.get(user=self.amir)
+        self.assertEqual(self.profile.credit, 500)
+
+    def test_get_cnt_following(self):
+        self.vahid, created = User.objects\
+            .get_or_create(username='vahid', email='a.ac@yahoo.com', password='1')
+        self.profile = Profile.objects.get(user=self.amir)
+
+        self.assertEqual(self.profile.get_cnt_following(), 0)
+
+    def test_get_cnt_followers(self):
+        self.profile = Profile.objects.get(user=self.amir)
+
+        self.assertEqual(self.profile.get_cnt_followers(), 0)
