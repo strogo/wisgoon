47badbd3414f91b632d78012271fb090625fb4bc
amir abbas ghorbani <a.abbasghorbani@gmail.com>
2015-12-09 14:34:37 +0330

    fix dashboard home

--- pin/views2/dashboard/tools.py
+++ pin/views2/dashboard/tools.py
@@ -12,53 +12,87 @@ from pin.model_mongo import MonthlyStats
 
 
 def today_new_users(today):
-    cnt_today_users = MonthlyStats.objects\
-        .filter(date=(today), object_type=MonthlyStats.USER).count()
+    try:
+        cnt_today_users = MonthlyStats.objects\
+            .get(date=(today), object_type=MonthlyStats.USER)
+        cnt_today_users = cnt_today_users.count
+    except:
+        cnt_today_users = 0
 
     return cnt_today_users
 
 
 def today_new_posts(today):
-    cnt_today_posts = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.POST).count()
+    try:
+        cnt_today_posts = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.POST)
+        cnt_today_posts = cnt_today_posts.count
+    except:
+        cnt_today_posts = 0
 
     return cnt_today_posts
 
 
 def today_likes(today):
-    cnt_today_likes = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.LIKE).count()
+    try:
+        cnt_today_likes = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.LIKE)
+        cnt_today_likes = cnt_today_likes.count
+    except:
+        cnt_today_likes = 0
 
     return cnt_today_likes
 
 
 def today_blocks(today):
-    cnt_today_blocks = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.BLOCK).count()
+    try:
+        cnt_today_blocks = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.BLOCK)
+        cnt_today_blocks = cnt_today_blocks.count
+    except:
+        cnt_today_blocks = 0
 
     return cnt_today_blocks
 
 
 def today_follow(today):
-    cnt_today_follow = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.FOLLOW).count()
+    try:
+        cnt_today_follow = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.FOLLOW)
+        cnt_today_follow = cnt_today_follow.count
+    except:
+        cnt_today_follow = 0
 
     return cnt_today_follow
 
 
 def today_view_pages(today):
-    today_view = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.VIEW).count()
+    try:
+        today_view = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.VIEW)
+        today_view = today_view.count
+    except:
+        today_view = 0
+
     return today_view
 
 
 def today_bills(today):
-    today_cnt_bills = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.BILL).count()
+    try:
+        today_cnt_bills = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.BILL)
+        today_cnt_bills = today_cnt_bills.count
+    except:
+        today_cnt_bills = 0
+
     return today_cnt_bills
 
 
 def today_comments(today):
-    today_cnt_bills = MonthlyStats.objects\
-        .filter(date=today, object_type=MonthlyStats.COMMENT).count()
-    return today_cnt_bills
+    try:
+        today_cnt_comments = MonthlyStats.objects\
+            .get(date=today, object_type=MonthlyStats.COMMENT)
+        today_cnt_comments = today_cnt_comments.count
+    except:
+        today_cnt_comments = 0
+    return today_cnt_comments
