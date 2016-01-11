a7bdcaf02de9d678b33318c81147e5cb755084e0
vchakoshy <vchakoshy@gmail.com>
2016-01-11 13:15:09 +0330

    fix app js routing

--- feedreader/media/dashboard/js/app.js
+++ feedreader/media/dashboard/js/app.js
@@ -12,32 +12,32 @@ app.config(function ($stateProvider, $urlRouterProvider) {
      $stateProvider
      .state('home', {
         url: "/",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/home.html",
+        templateUrl: "/media/dashboard/html/home.html",
         controller: "indexController"
     })
      .state('ads', {
         url: "/ads",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/post/ads.html",
+        templateUrl: "/media/dashboard/html/post/ads.html",
         controller: "adsController"
     })
      .state('logs', {
         url: "/logs",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/post/logs.html",
+        templateUrl: "/media/dashboard/html/post/logs.html",
         controller: "logsController"
     })
      .state('userActivity', {
         url: "/userActivity",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/post/user_activity.html",
+        templateUrl: "/media/dashboard/html/post/user_activity.html",
         controller: "activityUserController"
     })
      .state('categoreis', {
         url: "/categoreis",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/post/categories.html",
+        templateUrl: "/media/dashboard/html/post/categories.html",
         controller: "catstatController"
     })
      .state('reported', {
         url: "/reported",
-        templateUrl: "http://127.0.0.1:8000/media/dashboard/html/post/reported.html",
+        templateUrl: "/media/dashboard/html/post/reported.html",
         controller: "reportedController"
     })
      
