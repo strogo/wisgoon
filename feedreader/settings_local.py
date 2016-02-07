039e412ed85ae1f5782f1618afe47d0173b25048
chalist <mohammad.molana@gmail.com>
2016-02-07 22:11:06 +0330

    change media version

--- feedreader/settings_local.py
+++ feedreader/settings_local.py
@@ -9,7 +9,7 @@ THUMBNAIL_DEBUG = True
 DISPLAY_AD = False
 TEMPLATE_DEBUG = DEBUG
 REPORT_TYPE = {'PIN': 1, 'COMMENT': 2, 'RSS': 3}
-STATIC_VERSION = '5.8.3'
+STATIC_VERSION = '5.8.4'
 
 SITE_ROOT = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
