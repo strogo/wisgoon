2cec9b77f1138e360572f1fe45679f6cf303f1d0
vchakoshy <vchakoshy@gmail.com>
2016-03-10 11:09:08 +0330

    remove unused stats

--- pin/views2/dashboard/home.py
+++ pin/views2/dashboard/home.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
 
 @login_required
 def home(request):
+    if not request.user.is_superuser:
+        return HttpResponseRedirect('/')
     return render(request, 'dashboard/home.html')
