b3d0c44ab6d5690fb291f5e55646507346eb7074
vchakoshy <vchakoshy@gmail.com>
2016-04-06 16:53:46 +0430

    fix bug

--- pin/api6/auth.py
+++ pin/api6/auth.py
@@ -272,10 +272,23 @@ def register(request):
         return return_json_data(data)
 
     if user:
+        auth_login(request, user)
+
+        api_key, created = ApiKey.objects.get_or_create(user=user)
+
         data = {
             'status': True,
-            'message': _("User created successfully")
+            'message': _("User created successfully"),
+            'user': {
+                'token': api_key.key,
+                'id': user.id,
+                'avatar': get_avatar(user)
+            }
         }
+        # data = {
+        #     'status': True,
+        #     'message': _("User created successfully")
+        # }
         return return_json_data(data)
     else:
         data = {
