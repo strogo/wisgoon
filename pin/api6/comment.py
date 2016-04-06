13ba5612f0b25f9d8084a25287764ef0e45e9612
vchakoshy <vchakoshy@gmail.com>
2016-04-06 17:31:03 +0430

    fix bug on comments

--- pin/api6/comment.py
+++ pin/api6/comment.py
@@ -8,7 +8,8 @@ from pin.api6.tools import get_int, get_next_url,\
     get_comments, comment_objects_list, comment_item_json
 from pin.api6.http import return_json_data, return_not_found, return_un_auth,\
     return_bad_request
-from pin.tools import check_block
+from pin.tools import check_block, get_post_user_cache
+from pin.toolkit import check_auth
 from pin.context_processors import is_police
 
 
@@ -31,37 +32,45 @@ def comment_post(request, item_id):
 
 @csrf_exempt
 def add_comment(request, item_id):
-    token = request.GET.get('token', '')
-    if token:
-        current_user = AuthCache.id_from_token(token=token)
-        if not current_user:
-            return return_un_auth()
-    else:
-        return return_bad_request()
+    user = check_auth(request)
+    if not user:
+        return return_json_data({
+            'status': False,
+            'message': _('error in user validation')
+        })
 
     try:
-        post = Post.objects.get(id=get_int(item_id))
+        post = get_post_user_cache(post_id=get_int(item_id))
+        # post = Post.objects.get(id=get_int(item_id))
     except Post.DoesNotExist:
         return return_not_found()
 
     text = request.POST.get('comment', False)
     if not text:
-        return return_json_data({'status': False, 'message': _('Please Enter Your Comment')})
+        return return_json_data({
+            'status': False,
+            'message': _('Please Enter Your Comment')
+        })
 
     if check_block(user_id=post.user_id, blocked_id=request.user.id):
         if not is_police(request, flat=True):
-            return return_json_data({'status': False,
-                                     'message': _('This User Has Blocked You')})
+            return return_json_data({
+                'status': False,
+                'message': _('This User Has Blocked You')
+            })
     try:
         comment = Comments.objects.create(object_pk=post, comment=text,
-                                          user_id=get_int(current_user))
+                                          user_id=get_int(user.id),
+                                          ip_address=user._ip)
         comment_data = comment_item_json(comment)
         comment_data['status'] = True
         comment_data['message'] = _('Successfully was Create Comment.')
         return return_json_data(comment_data)
     except:
-        return return_json_data({'status': False,
-                                 'message': _('Unsuccessfully was Create Comment.')})
+        return return_json_data({
+            'status': False,
+            'message': _('Unsuccessfully was Create Comment.')
+        })
 
 
 def delete_comment(request, comment_id):
