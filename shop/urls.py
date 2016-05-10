455c278fc0ea5f23e316055966cb7543cff9a708
amir ghorbani <a.abbasghorbani@gmail.com>
2016-05-10 16:57:14 +0430

    add hesabit redirect url

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,19 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 
 urlpatterns = patterns('shop.views',
-    url(r'^$', 'home', name='shop-home'),
-    url(r'^product/(?P<product_id>\d+)$', 'product', name='shop-product'),
-    url(r'^cart/add/product/$', 'cart_add_product', name='shop-cart-add-product'),
-    url(r'^cart/remove/product/(?P<product_id>\d+)/$', 'cart_remove_product', name='shop-cart-remove-product'),
-    url(r'^cart/$', 'cart', name='shop-cart'),
-    url(r'^address/info/$', 'address', name='shop-address'),
-    url(r'^confirm/$', 'confirm', name='shop-confirm'),
-)
+                       url(r'^$', 'home', name='shop-home'),
+                       url(r'^product/(?P<product_id>\d+)$', 'product', name='shop-product'),
+
+                       url(r'^cart/add/product/$', 'cart_add_product',
+                           name='shop-cart-add-product'),
+
+                       url(r'^cart/remove/product/(?P<product_id>\d+)/$',
+                           'cart_remove_product',
+                           name='shop-cart-remove-product'),
+
+                       url(r'^cart/$', 'cart', name='shop-cart'),
+                       url(r'^address/info/$', 'address', name='shop-address'),
+                       url(r'^confirm/$', 'confirm', name='shop-confirm'),
+                       url(r'^hesabit/redirect/$', 'hesabit_redirect',
+                           name='shop-hesabit-redirect'),
+                       )
