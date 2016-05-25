799dc3692d9d9a7ad698860317ec4653c5fecbd7
vchakoshy <vchakoshy@gmail.com>
2016-05-25 13:02:48 +0430

    change cache server

--- feedreader/settings_production.py
+++ feedreader/settings_production.py
@@ -42,11 +42,11 @@ DATABASES = {
 CACHES = {
     'default': dict(
         BACKEND='django.core.cache.backends.memcached.MemcachedCache',
-        LOCATION=['79.127.125.98:11211'],
+        LOCATION=['79.127.125.104:11211'],
     ),
     'cache_layer': dict(
         BACKEND='django.core.cache.backends.memcached.MemcachedCache',
-        LOCATION=['79.127.125.98:11211'],
+        LOCATION=['79.127.125.104:11211'],
     )
 }
 
