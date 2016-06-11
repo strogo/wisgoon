6f15a4b8559256ac2e1358888a67cf92ab416b9e
vchakoshy <vchakoshy@gmail.com>
2016-06-11 14:58:55 +0430

    change style

--- push.sh
+++ push.sh
@@ -1,70 +1,70 @@
 #!/bin/sh
 
 sleep_f(){
-	printf "Waiting for reloading uwsgi"
-	for p in {1..20}
-	do
-		printf "."
-		sleep 1
-	done
+    printf "Waiting for reloading uwsgi"
+    for p in {1..20}
+    do
+        printf "."
+        sleep 1
+    done
 }
 
 send(){
-	git push origin devel
-	ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel && touch reload"
-	sleep_f
+    git push origin devel
+    ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel && touch reload"
+    sleep_f
 
-	git push eiffel devel
-	ssh wisgoon@79.127.125.99 "cd /home/wisgoon/www && git merge devel && touch reload"
-	sleep_f
+    git push eiffel devel
+    ssh wisgoon@79.127.125.99 "cd /home/wisgoon/www && git merge devel && touch reload"
+    sleep_f
 
-	git push mars devel
-	ssh wisgoon@79.127.125.98 "cd /home/wisgoon/www && git merge devel && touch reload"
-	sleep_f
+    git push mars devel
+    ssh wisgoon@79.127.125.98 "cd /home/wisgoon/www && git merge devel && touch reload"
+    sleep_f
 
-	# git push emsham devel
-	# ssh emsham@79.127.125.104 "cd /home/emsham/www && git merge devel && touch reload"
+    # git push emsham devel
+    # ssh emsham@79.127.125.104 "cd /home/emsham/www && git merge devel && touch reload"
 
-	#git push salam devel
-	#ssh salam@46.209.152.53 "cd /home/salam/www && git merge devel && touch reload"
+    #git push salam devel
+    #ssh salam@46.209.152.53 "cd /home/salam/www && git merge devel && touch reload"
 }
 
 jsend(){
-	git push origin devel
-	ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel"
+    git push origin devel
+    ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www && git merge devel"
 
-	git push eiffel devel
-	ssh wisgoon@79.127.125.99 "cd /home/wisgoon/www && git merge devel"
+    git push eiffel devel
+    ssh wisgoon@79.127.125.99 "cd /home/wisgoon/www && git merge devel"
 
-	git push mars devel
-	ssh wisgoon@79.127.125.98 "cd /home/wisgoon/www && git merge devel"
+    git push mars devel
+    ssh wisgoon@79.127.125.98 "cd /home/wisgoon/www && git merge devel"
 
-	# git push emsham devel
-	# ssh emsham@79.127.125.104 "cd /home/emsham/www && git merge devel"
+    # git push emsham devel
+    # ssh emsham@79.127.125.104 "cd /home/emsham/www && git merge devel"
 }
 
 case $1 in
-	
-	
-	commit)
-		git add .
-		git commit -m "change on file"
-		send
-	;;
-	
-	send)
-		send
+    
+    
+    commit)
+        git add .
+        git commit -m "change on file"
+        send
+    ;;
+    
+    send)
+        send
         ;;
 
     jsend)
-		# git add .
-		# git commit -m "change on file"
-		jsend
+        # git add .
+        # git commit -m "change on file"
+        jsend
         ;;
 
-	*)
-		echo "commit\t for autocommit"
-		echo "send\t for push and merge"
-	;;
+    *)
+        echo "commit\t for autocommit"
+        echo "send\t for push and merge"
+    ;;
 esac
 
