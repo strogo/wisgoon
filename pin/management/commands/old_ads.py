45c02b4321cec06590ef9fc10b453b7c91e48025
vchakoshy <vchakoshy@gmail.com>
2016-06-12 14:32:18 +0430

    add limit

--- pin/management/commands/old_ads.py
+++ pin/management/commands/old_ads.py
@@ -9,7 +9,7 @@ r_server = redis.Redis(settings.REDIS_DB, db=settings.REDIS_DB_NUMBER)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for ad in Ad.objects.filter(ended=True).all():
+        for ad in Ad.objects.filter(ended=True).all()[:10000]:
             key_name = "ad_{}".format(ad.id)
             print "going to remove " + key_name
             r_server.delete(key_name)
