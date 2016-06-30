907094d844bc2af34c485911b24c024a7f1fd1b6
vchakoshy <vchakoshy@gmail.com>
2016-06-30 22:49:10 +0430

    changes

--- feedreader/settings_devel.py
+++ feedreader/settings_devel.py
@@ -6,7 +6,7 @@ TEMPLATE_DEBUG = True
 THUMBNAIL_DEBUG = False
 DISPLAY_AD = True
 
-SITE_ID = 2
+SITE_ID = 1
 
 MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
 
@@ -22,20 +22,15 @@ ADMINS = (
 
 CASSANDRA_DB = '79.127.125.104'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'wisgoon',
-        'USER': 'wisgoon_user',
-        'PASSWORD': 'KP9CPby8jkOCw',
-        'HOST': '79.127.125.99',
-        'PORT': '',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'wisgoon',
+#         'USER': 'wisgoon_user',
+#         'PASSWORD': 'KP9CPby8jkOCw',
+#         'HOST': '79.127.125.99',
+#         'PORT': '',
+#     }
+# }
 
-ALLOWED_HOSTS = ['www.wisgoon.com', '*.wisgoon.com', 'wisgoon.com', "Sib-DL2",
-                 "127.0.0.1:3060", "127.0.0.1:3061", "127.0.0.1:3062",
-                 "127.0.0.1", "debian", "rokh01", "new.wisgoon.com",
-                 "wisweb1.com", "www.wisweb1.com",
-                 "wisweb2.com", "www.wisweb2.com",
-                 "wisweb3.com", "www.wisweb3.com"]
+ALLOWED_HOSTS = ALLOWED_HOSTS + ["new.wisgoon.com"]
