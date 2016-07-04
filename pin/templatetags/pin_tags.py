39fc20c280892ce55ec7e4b855c35e72ae6b41de
vchakoshy <vchakoshy@gmail.com>
2016-07-04 14:15:39 +0430

    changes

--- pin/templatetags/pin_tags.py
+++ pin/templatetags/pin_tags.py
@@ -58,7 +58,7 @@ millnames = ['', ' K', ' M', ' B', ' T']
 def millify(n):
     n = float(n)
     millidx = max(0, min(len(millnames) - 1,
-                         int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
+                  int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
 
     return '{:.0f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
 
