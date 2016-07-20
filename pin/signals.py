f0a8d5bc4043d6c1b2c149809323967cf5b4d6b1
amir ghorbani <a.abbasghorbani@gmail.com>
2016-07-20 12:08:43 +0430

    add signal

--- pin/signals.py
+++ pin/signals.py
@@ -2,16 +2,18 @@ from django.db import models
 
 from haystack import signals
 
-from pin.models import Post
+from pin.models import Post, Campaign
 from user_profile.models import Profile
 
 
 class MySignalProcessor(signals.BaseSignalProcessor):
     def setup(self):
         models.signals.post_save.connect(self.handle_save, sender=Post)
+        models.signals.post_save.connect(self.handle_save, sender=Campaign)
         models.signals.post_save.connect(self.handle_save, sender=Profile)
         # models.signals.post_save.connect(self.handle_save, sender=Comments)
 
         models.signals.post_delete.connect(self.handle_delete, sender=Post)
         models.signals.post_delete.connect(self.handle_delete, sender=Profile)
+        models.signals.post_delete.connect(self.handle_delete, sender=Campaign)
         # models.signals.post_delete.connect(self.handle_delete, sender=Comments)
