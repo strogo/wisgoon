c42ba3aca64e3b3b1869c0d0fa45b2e003994776
amir ghorbani <a.abbasghorbani@gmail.com>
2016-08-17 16:04:38 +0430

    change mopdels casper

--- pin/models_casper.py
+++ pin/models_casper.py
@@ -48,7 +48,7 @@ class UserStream(Model):
 #     user_id = columns.Integer(primary_key=True)
 
 try:
-    slist = ['127.0.0.1']
+    slist = ['79.127.125.104']
     connection.setup(slist, "wisgoon", protocol_version=3)
     management.create_keyspace_simple("wisgoon", replication_factor=1)
 
