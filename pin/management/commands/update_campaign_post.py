e9b755dae10aefcb326551e7ad3ab05d62039cf1
amir ghorbani <a.abbasghorbani@gmail.com>
2016-09-19 16:49:23 +0430

    fix update campaign

--- pin/management/commands/update_campaign_post.py
+++ pin/management/commands/update_campaign_post.py
@@ -6,16 +6,22 @@ from pin.search_indexes import PostIndex
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        campaigns = Campaign.objects.filter(is_current=True, expired=False).order_by('-id')
+        campaigns = Campaign.objects\
+            .filter(is_current=True, expired=False)\
+            .order_by('-id')
+
         for campaign in campaigns:
+            print campaign.id
             campaign_tags = campaign.tags
             tags = campaign_tags.split(',')
             tags.append(campaign.primary_tag)
             start_date = campaign.start_date.strftime("%s")
             end_date = campaign.end_date.strftime("%s")
 
-            posts = SearchQuerySet().models(Post).filter(tags__in=tags)\
-                .filter(timestamp_i__lte=end_date).filter(timestamp_i__gte=start_date)\
+            posts = SearchQuerySet().models(Post)\
+                .filter(tags__in=tags,
+                        timestamp_i__lte=end_date,
+                        timestamp_i__gte=start_date)\
                 .order_by('-cnt_like_i')
 
             for post in posts:
