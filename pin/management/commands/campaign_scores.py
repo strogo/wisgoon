08b5932f24501d4b06c562cade7e7fe2f068fd6d
vchakoshy <vchakoshy@gmail.com>
2016-09-20 19:19:29 +0430

    fix bug

--- pin/management/commands/campaign_scores.py
+++ pin/management/commands/campaign_scores.py
@@ -5,31 +5,29 @@ from pin.models import Post, Campaign
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        campaigns = Campaign.objects.get(id=4)
+        camp = Campaign.objects.get(id=4)
+        print camp.id
+        campaign_tags = camp.tags
+        tags = campaign_tags.split(',')
+        tags.append(camp.primary_tag)
+        start_date = camp.start_date.strftime("%s")
+        end_date = camp.end_date.strftime("%s")
 
-        for campaign in campaigns:
-            print campaign.id
-            campaign_tags = campaign.tags
-            tags = campaign_tags.split(',')
-            tags.append(campaign.primary_tag)
-            start_date = campaign.start_date.strftime("%s")
-            end_date = campaign.end_date.strftime("%s")
+        posts = SearchQuerySet().models(Post)\
+            .filter(tags__in=tags,
+                    timestamp_i__gte=start_date,
+                    timestamp_i__lte=end_date)
 
-            posts = SearchQuerySet().models(Post)\
-                .filter(tags__in=tags,
-                        timestamp_i__gte=start_date,
-                        timestamp_i__lte=end_date)
+        user_obj = {}
 
-            user_obj = {}
-
-            for post in posts:
-                try:
-                    post_obj = Post.objects.get(id=post.pk)
-                    u = post_obj.user_id
-                    if u in user_obj:
-                        user_obj[u] += post_obj.cnt_like
-                    else:
-                        user_obj[u] = post_obj.cnt_like
-                except Exception:
-                    pass
-            print user_obj
+        for post in posts:
+            try:
+                post_obj = Post.objects.get(id=post.pk)
+                u = post_obj.user_id
+                if u in user_obj:
+                    user_obj[u] += post_obj.cnt_like
+                else:
+                    user_obj[u] = post_obj.cnt_like
+            except Exception:
+                pass
+        print user_obj
