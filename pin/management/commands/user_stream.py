c011fd57baa88f8b8ab82a3df6d6574791b686d2
vchakoshy <vchakoshy@gmail.com>
2016-09-26 19:40:36 +0330

    fix bug

--- pin/management/commands/user_stream.py
+++ pin/management/commands/user_stream.py
@@ -1,8 +1,13 @@
+import redis
+
 from django.core.management.base import BaseCommand
+from django.conf import settings
 from django.contrib.auth.models import User
 
 from pin.models_casper import UserStream
-from pin.models import Post, Follow
+from pin.api6.tools import post_item_json
+
+r_server = redis.Redis(settings.REDIS_DB, db=settings.REDIS_DB_NUMBER)
 
 
 class Command(BaseCommand):
@@ -11,11 +16,14 @@ class Command(BaseCommand):
         us = UserStream()
         for user in users:
             print "user:", user.id
-            q = Follow.objects.filter(follower_id=user.id)
-            for f in q:
-                pid_list = Post.objects.filter(user_id=f.following_id)\
-                    .only("id")\
-                    .values_list("id", flat=True)\
-                    .order_by("-id")[:100]
-                us.follow(user.id, pid_list, f.following_id)
+            user_stream = "{}_{}".format(settings.USER_STREAM, int(user.id))
+            pl = r_server.lrange(user_stream, 0, 1000)
+            if not pl:
+                continue
+
+            for pll in pl:
+                pll_id = int(pll)
+                ob = post_item_json(pll_id)
+                us.add_post(user.id, pll_id, ob['user']['id'])
+
             us.ltrim(user.id, 1000)
