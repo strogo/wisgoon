4a84b341d34c15a9a5a4c7270fef07884f881043
amir ghorbani <a.abbasghorbani@gmail.com>
2016-09-28 15:47:18 +0330

    bug fix

--- pin/api6/notification.py
+++ pin/api6/notification.py
@@ -63,7 +63,7 @@ def notif(request):
             cnt_requests = follow_requests.count()
             if cnt_requests > 0:
                 last_follow_req = follow_requests[0]
-                user_obj = get_simple_user_object(last_follow_req.user)
+                user_obj = get_simple_user_object(last_follow_req.user.id)
 
             data['follow_requests'] = {'user': user_obj,
                                        'cnt_requests': cnt_requests}
