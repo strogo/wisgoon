dae81e1537652977b3c17f5eaa5fcc3be6f7600f
amir ghorbani <a.abbasghorbani@gmail.com>
2016-09-28 11:04:58 +0330

    fix field name

--- pin/api6/tools.py
+++ pin/api6/tools.py
@@ -148,7 +148,7 @@ def get_simple_user_object(current_user, user_id_from_token=None, avatar=64):
     user_info['related'] = {}
     user_info['follow_by_user'] = False
     user_info['block_by_user'] = False
-    user_info['relation_blocked'] = False
+    user_info['user_blocked_me'] = False
 
     user_info['related']['posts'] = abs_url(reverse('api-6-post-user',
                                                     kwargs={
@@ -165,7 +165,7 @@ def get_simple_user_object(current_user, user_id_from_token=None, avatar=64):
             .filter(user_id=user_id_from_token, blocked_id=current_user)\
             .exists()
 
-        user_info['user_by_block'] = Block.objects\
+        user_info['user_blocked_me'] = Block.objects\
             .filter(user_id=current_user,
                     blocked_id=user_id_from_token)\
             .exists()
