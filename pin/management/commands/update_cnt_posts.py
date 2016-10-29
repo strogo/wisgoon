6bf2dbd8b3977a599c28c1d84bb5553f0834ebc6
amir ghorbani <a.abbasghorbani@gmail.com>
2016-10-29 13:02:29 +0330

    update user cnt post

---
+++ pin/management/commands/update_cnt_posts.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+
+from pin.models import Post
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        users = User.objects.only('id').order_by('-id')
+        for user in users:
+            cnt_post = Post.objects.only('id')\
+                .filter(user_id=user.id)\
+                .count()
+            user.profile.cnt_post = cnt_post
+            user.profile.save()
+            print "user {} updated".format(user.id)
