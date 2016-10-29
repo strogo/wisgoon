3e0828a1c91c822060ceb77e7e5677aa4a846631
amir ghorbani <a.abbasghorbani@gmail.com>
2016-10-29 15:11:19 +0330

    fix translate fix update cnt post

--- pin/management/commands/update_cnt_posts.py
+++ pin/management/commands/update_cnt_posts.py
@@ -6,11 +6,14 @@ from django.contrib.auth.models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        users = User.objects.only('id').order_by('-id')
+        users = User.objects.only('id').filter(id__lt=1743656).order_by('-id')
         for user in users:
             cnt_post = Post.objects.only('id')\
                 .filter(user_id=user.id)\
                 .count()
-            user.profile.cnt_post = cnt_post
-            user.profile.save()
-            print "user {} updated".format(user.id)
+            try:
+                user.profile.cnt_post = cnt_post
+                user.profile.save()
+                print "user {} updated".format(user.id)
+            except:
+                pass
