630d2c4f61d03efd3824aa7193a5827dfc51ced3
vchakoshy <vchakoshy@gmail.com>
2016-11-06 14:39:59 +0330

    remove node 104 from cassandra cluster

--- pin/models_casper.py
+++ pin/models_casper.py
@@ -16,11 +16,11 @@ class CassandraModel():
         global isConnected, session
         if not isConnected:
             if settings.DEVEL_BRANCH:
-                cluster = Cluster(['79.127.125.104', '79.127.125.99'])
+                cluster = Cluster(['79.127.125.99'])
             elif settings.DEBUG:
                 cluster = Cluster(['127.0.0.1'])
             else:
-                cluster = Cluster(['79.127.125.104', '79.127.125.99'])
+                cluster = Cluster(['79.127.125.99'])
             session = cluster.connect("wisgoon")
             isConnected = True
 
