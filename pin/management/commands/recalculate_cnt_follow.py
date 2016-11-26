e8b0ae2c6640a8b01abbfaef2d56ce1197d9f8e2
amir ghorbani <a.abbasghorbani@gmail.com>
2016-11-26 16:38:41 +0330

    fix recalculate command

--- pin/management/commands/recalculate_cnt_follow.py
+++ pin/management/commands/recalculate_cnt_follow.py
@@ -9,8 +9,8 @@ class Command(BaseCommand):
         users = User.objects.only('id').all()
 
         for user in users:
-            cnt_following = Follow.objects.filter(following_id=user.id).count()
-            cnt_followers = Follow.objects.filter(follower_id=user.id).count()
+            cnt_followers = Follow.objects.filter(following_id=user.id).count()
+            cnt_following = Follow.objects.filter(follower_id=user.id).count()
 
             Profile.objects.filter(user_id=user.id)\
                 .update(cnt_following=cnt_following,
