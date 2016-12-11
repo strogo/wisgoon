88fa5e8f00c9cc173ae7d2aacde22d1d79640ebb
vchakoshy <vchakoshy@gmail.com>
2016-12-11 12:31:39 +0330

    fix cache days

--- pin/api6/decorators.py
+++ pin/api6/decorators.py
@@ -5,7 +5,7 @@ def cache_days(age):
         def wrapper(*args, **kwargs):
             expiry_time = datetime.datetime.utcnow() + datetime.timedelta(age)
             rjd = function(*args, **kwargs)
-            rjd['Cache-Control'] = 'public'
+            rjd['Cache-Control'] = "public, max-age="+ str(age * 24 * 60 * 60)
             rjd["Expires"] = expiry_time.strftime("%a, %d %b %Y %H:%M:%S GMT")
             return rjd
         return wrapper
