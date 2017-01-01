d05d50b37cb417c4283e0dc4baacbb3ca3a3a3ec
amir ghorbani <a.abbasghorbani@gmail.com>
2017-01-01 15:22:41 +0330

    commnet top post code

--- pin/models_redis.py
+++ pin/models_redis.py
@@ -319,15 +319,15 @@ class LikesRedis(object):
             disliked = False
 
         lbs.execute()
-        if date:
-            from pin.api6.tools import timestamp_to_local_datetime
-            from pin.models_stream import RedisTopPostStream
+        # if date:
+        #     from pin.api6.tools import timestamp_to_local_datetime
+        #     from pin.models_stream import RedisTopPostStream
 
-            convert_date = timestamp_to_local_datetime(int(date))
-            top_stream = RedisTopPostStream()
-            top_stream.add_post(post_id=self.postId,
-                                cnt_like=self.cntlike(),
-                                date=convert_date)
+        #     convert_date = timestamp_to_local_datetime(int(date))
+        #     top_stream = RedisTopPostStream()
+        #     top_stream.add_post(post_id=self.postId,
+        #                         cnt_like=self.cntlike(),
+        #                         date=convert_date)
 
         return liked, disliked, self.cntlike()
 
