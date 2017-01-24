5382e31e78f43976e6c2c415faeef9ef8948a540
amir ghorbani <a.abbasghorbani@gmail.com>
2017-01-24 10:02:07 +0330

    clean code

--- pin/models.py
+++ pin/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import emoji
-import StringIO
 import hashlib
 import os
 import re
@@ -1346,7 +1345,6 @@ class Comments(models.Model):
                 .update(cnt_comment=F('cnt_comment') + 1)
 
         # self.comment = emoji.demojize(self.comment)[:2048]
-        # sio = StringIO.StringIO(self.comment)
         self.comment = emoji.demojize(self.comment[:2048])
 
         comment_cache_name = "com_{}".format(int(self.object_pk_id))
