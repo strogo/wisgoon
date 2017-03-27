a92100972a5181fca32fca4e56d0bd7574c87bec
amir ghorbani <a.abbasghorbani@gmail.com>
2017-03-27 10:31:41 +0430

    fix merge

--- push-final.sh
+++ push-final.sh
@@ -2,10 +2,10 @@
 
 send(){
 
-	# git push origin devel
+	git push origin devel
  #    ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www/ && git merge devel && touch reload"
 
-    git push neptune devel
+    # git push neptune devel
     ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/ && git merge devel && touch reload"
 }
 
