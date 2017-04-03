e92ad297b3fef3ade5e66a6d603da71607279069
vchakoshy <vchakoshy@gmail.com>
2017-04-03 18:35:52 +0430

    fix bug

--- push-final.sh
+++ push-final.sh
@@ -5,7 +5,7 @@ send(){
 	# git push origin devel
  #    ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www/ && git merge devel && touch reload"
 
-    git push neptune devel
+    git push origin devel
     ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/ && git merge devel && touch reload"
 }
 
