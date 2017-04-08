d2c6a563a2cb33a3839be2ca8b813ed9913b6132
vchakoshy <vchakoshy@gmail.com>
2017-04-08 16:01:57 +0430

    add jsend

--- push-final.sh
+++ push-final.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 
 send(){
-
 	git push origin devel
- #    ssh wisgoon@79.127.125.146 "cd /home/wisgoon/www/wisgoon.com/www/ && git merge devel && touch reload"
-
-    # git push neptune devel
-
     ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/ && git merge devel && touch reload"
+}
+
+jsend(){
+	git push origin devel
+    ssh wisgoon@79.127.125.104 "cd /home/wisgoon/www/ && git merge devel"
 }
 
 case $1 in
     send)
         send
         ;;
+
+    jsend)
+		jsend
+		;;
 
     *)
         echo "send\t for push and merge"
