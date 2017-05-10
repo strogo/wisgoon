eda81df21cc9871572393553e366155b54310b82
amir ghorbani <a.abbasghorbani@gmail.com>
2017-05-10 15:15:38 +0430

    change hashtag api

--- pin/api6/post.py
+++ pin/api6/post.py
@@ -390,81 +390,6 @@ def promoted(request):
     return return_json_data(data)
 
 
-def hashtag(request, tag_name):
-    token = request.GET.get('token', '')
-    query = tag_name.strip()
-    before = get_int(request.GET.get('before', 0))
-
-    row_per_page = 20
-    cur_user = None
-    data = {}
-    data['meta'] = {'limit': 20,
-                    'next': "",
-                    'total_count': 0}
-    data['objects'] = []
-    # return return_json_data(data)
-
-    # if query:
-
-    #     results = SearchQuerySet().models(Post).filter(tags=query)
-
-    #     data['meta']['total_count'] = results.count()
-
-    #     cur_user = AuthCache.id_from_token(token=token)
-    #     posts = []
-    #     rs = results.order_by('-timestamp_i')[before:before + row_per_page]
-    #     for p in rs:
-    #         try:
-    #             pp = int(p.object.id)
-    #             posts.append(pp)
-    #         except:
-    #             pass
-
-    #     data['objects'] = get_objects_list(posts, cur_user_id=cur_user,
-    #                                        r=request)
-
-    #     data['meta']['next'] = get_next_url(url_name='api-6-post-hashtag',
-    #                                         before=before + row_per_page,
-    #                                         token=token,
-    #                                         url_args={'tag_name': query}
-    #                                         )
-    #     return return_json_data(data)
-    # else:
-    #     return return_bad_request()
-
-    if query:
-
-        ps = ESPosts()
-        results, total_count = ps.search_tags(text=query,
-                                              offset=before,
-                                              limit=row_per_page)
-
-        data['meta']['total_count'] = total_count
-
-        cur_user = AuthCache.id_from_token(token=token)
-        posts = []
-        for p in results:
-            try:
-                pp = int(p.id)
-                posts.append(pp)
-            except Exception:
-                pass
-
-        if posts:
-            data['objects'] = get_objects_list(posts,
-                                               cur_user_id=cur_user,
-                                               r=request)
-
-            data['meta']['next'] = get_next_url(url_name='api-6-post-hashtag',
-                                                before=before + row_per_page,
-                                                token=token,
-                                                url_args={'tag_name': query}
-                                                )
-        return return_json_data(data)
-    else:
-        return return_bad_request()
-
-
 @csrf_exempt
 @system_writable
 def delete(request, item_id):
@@ -1290,3 +1215,77 @@ def item(request, item_id):
     #     return return_not_found()
 
     return return_json_data(data)
+
+
+def hashtag(request, tag_name):
+    token = request.GET.get('token', None)
+    before = int(request.GET.get('before', 0))
+    # query = tag_name.strip()
+    # row_per_page = 20
+    # cur_user = None
+
+    data = {}
+    data['meta'] = {'limit': 20,
+                    'next': "",
+                    'total_count': 0}
+    data['objects'] = []
+
+    payload = {}
+    if token:
+        payload['token'] = token
+    if before:
+        payload['before'] = token
+
+    if settings.DEBUG:
+        url = "http://127.0.0.1:8801/v7/post/hashtag/{}/"
+    else:
+        url = "http://test.wisgoon.com/v7/post/hashtag/{}/"
+
+    url = url.format(tag_name)
+
+    # Get choices post
+    s = requests.Session()
+    res = s.get(url, params=payload, headers={'Connection': 'close'})
+
+    if res.status_code == 200:
+        try:
+            data = json.loads(res.content)
+        except:
+            return return_not_found()
+    else:
+        return return_not_found()
+
+    return return_json_data(data)
+
+    # if query:
+
+    #     ps = ESPosts()
+    #     results, total_count = ps.search_tags(text=query,
+    #                                           offset=before,
+    #                                           limit=row_per_page)
+
+    #     data['meta']['total_count'] = total_count
+
+    #     cur_user = AuthCache.id_from_token(token=token)
+    #     posts = []
+    #     for p in results:
+    #         try:
+    #             pp = int(p.id)
+    #             posts.append(pp)
+    #         except Exception:
+    #             pass
+
+    #     if posts:
+    #         data['objects'] = get_objects_list(posts,
+    #                                            cur_user_id=cur_user,
+    #                                            r=request)
+
+    #         data['meta']['next'] = get_next_url(url_name='api-6-post-hashtag',
+    #                                             before=before + row_per_page,
+    #                                             token=token,
+    #                                             url_args={'tag_name': query}
+    #                                             )
+
+    #     return return_json_data(data)
+    # else:
+    #     return return_bad_request()
