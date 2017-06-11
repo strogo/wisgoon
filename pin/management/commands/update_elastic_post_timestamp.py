5366dde0b44f1ce36e54a1dd15e9a027c7ea8693
amir ghorbani <a.abbasghorbani@gmail.com>
2017-06-11 13:22:08 +0430

    create update elastic post timestamp managment

---
+++ pin/management/commands/update_elastic_post_timestamp.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from pin.models import Post
+from django.conf import settings
+from elasticsearch import Elasticsearch
+from elasticsearch import exceptions
+
+
+INDEX_POST = 'wis-posts'
+
+
+es = Elasticsearch(settings.ES_HOSTS)
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+
+        limit = 100
+        offset = 0
+        last_post = Post.objects.only('id').last()
+
+        while offset < last_post.id:
+            posts = Post.objects.filter(id__range=(offset, offset + limit))
+            for p in posts:
+                timestamp = int(p.create.strftime("%s"))
+                update_timestamp(p.id, timestamp)
+            offset = offset + limit
+            print offset
+
+
+def update_timestamp(post_id, timestamp):
+    try:
+        es.update(id=post_id,
+                  doc_type='post',
+                  index=INDEX_POST,
+                  body={"doc": {"timestamp": timestamp}},
+                  retry_on_conflict=5)
+    except exceptions.TransportError:
+        print "error"
+    except Exception as e:
+        print str(e)
