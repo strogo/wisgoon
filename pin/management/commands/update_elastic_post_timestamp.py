0baaa5916573ae43749e32550089ab8d5145a6b0
amir ghorbani <a.abbasghorbani@gmail.com>
2017-06-12 17:31:27 +0430

    fix offset

--- pin/management/commands/update_elastic_post_timestamp.py
+++ pin/management/commands/update_elastic_post_timestamp.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         limit = 100
-        offset = 19000000
+        offset = 19700000
         last_post = Post.objects.only('id').last()
 
         while offset < last_post.id:
